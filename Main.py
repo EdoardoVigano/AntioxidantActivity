41bc05a3b646c028bb570952b8c5922e5157cf54
EdoardoVigano <edoardovigano4@gmail.com>
2024-12-23 12:00:20 +0100

    version 3 models V_3

--- Main.py
+++ Main.py
@@ -4,10 +4,11 @@ import os
 from mordred import Calculator, descriptors, error
 
 from rdkit import Chem
+from rdkit.Chem.Descriptors import MolWt
 import numpy as np
 import argparse
 import sys
-
+from datetime import datetime
 
 
 def mordred_calculator(dataset:pd.DataFrame):
@@ -38,24 +39,29 @@ def check_smiles(smiles):
 def pipeline_model_importer():
     
     # import model and pipeline
-    with open(os.path.join(os.getcwd(), 'models', 'model_extratrees.pkl'), 'rb') as f:
+    with open(os.path.join(os.getcwd(), 'models', 'alldata_model_Antioxidant_DPPH30MIN_extra_trees_regressor.pkl'), 'rb') as f:
         model1 = pickle.load(f)
 
-    with open(os.path.join(os.getcwd(),'models', 'model_xgb.pkl'), 'rb') as f:
+    with open(os.path.join(os.getcwd(),'models', 'alldata_model_Antioxidant_DPPH30MIN_xgb_regressor.pkl'), 'rb') as f:
         model2 = pickle.load(f)
+    
+    with open(os.path.join(os.getcwd(),'models', 'alldata_model_Antioxidant_DPPH30MIN_gradient_boosting_regressor.pkl'), 'rb') as f:
+        model3 = pickle.load(f)
 
-    with open(os.path.join(os.getcwd(), 'pipeline_and_AD', 'pipeline.pkl'), 'rb') as f:
+    with open(os.path.join(os.getcwd(), 'pipeline_and_AD', 'pipeline_Antioxidant_DPPH30MIN.pkl'), 'rb') as f:
         pipeline = pickle.load(f)
 
     with open(os.path.join(os.getcwd(), 'pipeline_and_AD', 'AD_clf.pkl'), 'rb') as f:
         ad = pickle.load(f)
         
-    return model1, model2, pipeline, ad
+    return model1, model2, model3, pipeline, ad
 
 def files_importer():
     parser = argparse.ArgumentParser(description='Antioxidant assesment')
-    parser.add_argument('--smiles', required=False, help='Specify target SMILES')
-    parser.add_argument('--filename', required=False, help='Specify xlsx file with molecules to predict')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--smiles', required=False, help='Specify target SMILES')
+    group.add_argument('--filename', required=False, help='Specify xlsx file with molecules to predict')
+    parser.add_argument('--summary', default=None, help='Specify if you want prediction from all models or only the consensus (default: summary=None) ones')
     
     args = parser.parse_args()
     
@@ -65,12 +71,12 @@ def files_importer():
         input_value = args.filename
         
     print(f"Input value: {input_value}")
-    return input_value
+    return input_value, args.summary
 
 
 if __name__ == '__main__':
     print("Antioxidant Model:\nExtra trees model to predict IC50 (log(ug/ml))")
-    input_value = files_importer()
+    input_value, summary = files_importer()
     
     if ".xlsx" not in input_value: 
         df = pd.DataFrame([input_value], columns=['SMILES'])
@@ -78,21 +84,44 @@ if __name__ == '__main__':
         df = pd.read_excel(input_value)
         
     # import data for testing
-    model1, model2, pipeline, ad = pipeline_model_importer()
+    model1, model2, model3, pipeline, ad = pipeline_model_importer()
     # Calculate molecular descriptors
     print("Molecular descriptors calculation....\n")
     data = mordred_calculator(df)
     print("MDs tranformation...\n")
     data_input = pd.DataFrame(pipeline.transform(data.loc[:, pipeline.feature_names_in_]), columns = pipeline.feature_names_in_)
     
-    
     print("Model Assessment...")
+    mw_calc = [MolWt(Chem.MolFromSmiles(smi)) for smi in df['SMILES']]
+    df['Predictions_ETR [-log(IC50) M]'] = model1.predict(data_input.loc[:, model1.feature_names_in_])
+    df['Predictions_ETR [mg/L]'] = [(10**-(c))*mw_*1000 for c, mw_ in zip(df['Predictions_ETR [-log(IC50) M]'], mw_calc)]
 
-    df['Predictions_extratrees'] = model1.predict(data_input.loc[:, model1.feature_names_in_])
-    df['Predictions_XGB'] = model2.predict(data_input.loc[:, model2.feature_names_in_])
+    df['Predictions_XGB [-log(IC50) M]'] = model2.predict(data_input.loc[:, model2.feature_names_in_])
+    df['Predictions_XGB [mg/L]'] = [(10**-(c))*mw_*1000 for c, mw_ in zip(df['Predictions_XGB [-log(IC50) M]'], mw_calc)]
+
+    df['Predictions_GB [-log(IC50) M]'] = model3.predict(data_input.loc[:, model3.feature_names_in_])
+    df['Predictions_GB [mg/L]'] = [(10**-(c))*mw_*1000 for c, mw_ in zip(df['Predictions_GB [-log(IC50) M]'], mw_calc)]
+
+    
+    df['Consensus [-log(IC50) M]'] = [np.mean([y1, y2, y3]) for y1, y2, y3 in zip(df['Predictions_ETR [-log(IC50) M]'], df['Predictions_XGB [-log(IC50) M]'], df['Predictions_GB [-log(IC50) M]'])]
+    df['Interval [-log(IC50) M]'] = [np.std([y1, y2, y3]) for y1, y2, y3 in zip(df['Predictions_ETR [-log(IC50) M]'], df['Predictions_XGB [-log(IC50) M]'], df['Predictions_GB [-log(IC50) M]'])]
+    
+    df['Consensus [mg/L]'] = [np.mean([y1, y2, y3]) for y1, y2, y3 in zip(df['Predictions_ETR [mg/L]'], df['Predictions_XGB [mg/L]'], df['Predictions_GB [mg/L]'])]
+    df['Interval [mg/L]'] = [np.std([y1, y2, y3]) for y1, y2, y3 in zip(df['Predictions_ETR [mg/L]'], df['Predictions_XGB [mg/L]'], df['Predictions_GB [mg/L]'])]
+
+    
+    df = round(df, 3)
+    
+    df['Consensus AND Uncertanty [mg/L]'] = [f"{str(y)} \u00B1 {i}" for y, i in zip(df['Consensus [mg/L]'], df['Interval [mg/L]'])] 
     df['Applicability Domain'] = ad.predict(data_input.loc[:, model2.feature_names_in_])
-    df.to_excel('predictions.xlsx')
-    print(df)
+    current_date = datetime.now().strftime("%d_%m_%Y")# ("%Y-%m-%d")
+    if summary:
+        df.loc[:, ['Consensus AND Uncertanty [mg/L]', 'Applicability Domain']].to_excel(f'Summary_predictions_{current_date}.xlsx')
+        print(df)
+    else:
+        df.to_excel(f'Predictions_{current_date}.xlsx')
+        print(df)
+    
     
     
     
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # AntioxidantActivity
 Antioxidant Activity predction IC50
+
+
+create the AntioxidantDPPH env
+Dependencies ...
+
+
+To run:
+
+1. Command:
+    python Main.py --file [add file name] or --smiles [write single SMILES] [optional]: --summary 1
+
+    key: 
+        --file: path of file to predict the antioxidant activity must have column named SMILES (batch functionality)
+        OR
+        --smiles: write single SMILES to predic (single molecule functionality)
+
+        OPTIONAL: set summary to one
+        --summary 1 to have only the consensus prediction and uncertanty value.
+        [default] --summary None all model predictions are reported
+
+
+2. examples:
+    python Main.py --smiles c1ccccc1CCN --summary 1
+    python Main.py --file test.xlsx --summary 1
+    python Main.py --file test.xlsx 
